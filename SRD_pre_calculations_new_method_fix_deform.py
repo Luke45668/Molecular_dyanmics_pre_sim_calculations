51569fb76eda482e0351fdbc74a2bb3bfd986eec
Luke45668 <99890053+Luke45668@users.noreply.github.com>
2024-02-06 15:27:20 +0000

    added Myriad production

--- SRD_pre_calculations_new_method_fix_deform.py
+++ SRD_pre_calculations_new_method_fix_deform.py
@@ -53,7 +53,8 @@ real_fluid_density = 764.95 # H20
 T_K=307.65 # fluid temp at density listed
 #%% calcuating lengthscale coefficient 
 n_particle=2
-phi=np.array([0.05,0.03,0.01,0.008,0.005,0.003,0.002,0.0015,0.0013,0.0012])
+phi=np.array([0.2,0.1,0.05,0.03,0.02,0.01,0.008,0.005,0.003,0.002,0.0015,0.0013,0.0012])
+#phi=np.array([0.0012,0.0010,0.0008,0.0006])
 fina_size=300 # need to increase this if you want bigger boxes
 box_size_bar=np.linspace(1,fina_size,fina_size)
 
@@ -116,7 +117,7 @@ k_b=1.38e-23
 collision_cell_size_bar= 1
 fluid_particle_mass_bar= 1 
 simulation_temp_bar=1
-fluid_particle_number_density= 5 
+fluid_particle_number_density= 10 #5
 energy_scale= T_K * k_b / simulation_temp_bar
 r_particle=2.5e-5
 lengthscale= k_for_each_phi * r_particle
@@ -181,7 +182,7 @@ print("lengthscale",lengthscale)
 print("mass scale",mass_scale)
 print("energy scale",energy_scale)
 #print(collision_time_positive_bar)
-#print(collision_time_negative_bar)
+print('collision_time_negative_bar',collision_time_negative_bar)
 print("collision ratio",collision_ratio)
 print("box size",box_size_bar)
 print("srd count",srd_count)
@@ -219,7 +220,7 @@ def gamma_2(M):
     return gamma_2
 
 
-M=np.array([5,100,1000])
+M=np.array([5,10,100,1000])
 gamma_1= gamma_1(M)
 gamma_2= gamma_2(M)
 gamma_1_2=gamma_1* gamma_2 *(1/18)
@@ -284,11 +285,18 @@ plt.show()
 
 
  
-#%% KATHLEEN paths 
+#%% Producing scripts for kathleen
 ##################
 Path_2_shell_scirpts='/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/Shell_scripts_for_KATHLEEN'
-abs_path_2_lammps_exec='/home/ucahlrl/simulation_run_folder/lammps-23Jun2022/build_kathleen/lmp_mpi'
-abs_path_2_lammps_script='/home/ucahlrl/simulation_run_folder/pure_srd_fix_deform.file'
+#Kathleen path
+#abs_path_2_lammps_exec='/home/ucahlrl/simulation_run_folder/lammps-23Jun2022/build_kathleen/lmp_mpi'
+#abs_path_2_lammps_script='/home/ucahlrl/simulation_run_folder/pure_srd_equilibrium_hdf5_dump.file'
+
+
+abs_path_2_lammps_exec='/Volumes/Backup\ Plus\ 1/PhD_/Rouse\ Model\ simulations/Using\ LAMMPS\ imac/lammps-23Jun2022/build_imac_hdf5/lmp_mpi'
+abs_path_2_lammps_script='/Volumes/Backup\ Plus\ 1/PhD_/Rouse\ Model\ simulations/Using\ LAMMPS\ imac/LSC/pure_srd_equilibrium_hdf5_dump.file'
+
+
 Path_2_generic='/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/Shell_scripts_for_KATHLEEN'
 data_transfer_instructions=''
 extra_code=''
@@ -309,18 +317,19 @@ thermo_freq=1000
 erate_=np.array([0])
 VP_ave_freq=10000
 i_=0
-j_=10
+j_=4
 Path_2_generic='/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/Shell_scripts_for_KATHLEEN'
 hypthread='2'
-fluid_name='mpcpure'
+box_size_index=1
+fluid_name='M_10_'+str(box_size_bar[box_size_index])+'_'
 max_cores= 80
-box_size_index=4
 
 
-realisation_index_=[1,2,3,4,5,6,7,8,9,10]
+
+realisation_index_=[1,2,3,4]
 
 ram_requirement='2G'
-wall_time='2:00:00'
+wall_time='10:00:00'
 np_req=str(max_cores)
 phi_= str(phi[box_size_index])
  # for one KATHLEEN node 
@@ -387,4 +396,104 @@ def sim_file_prod_fix_deform_pure_individual_kathleen(nu_bar,erate_,var_choice_1
 # %%
 sim_file_prod_fix_deform_pure_individual_kathleen(nu_bar,erate_,var_choice_1,var_choice_2,hypthread,data_transfer_instructions,extra_code,wd_path,np_req,num_task_req,tempdir_req,wall_time,ram_requirement,realisation_index_,VP_ave_freq,abs_path_2_lammps_exec,abs_path_2_lammps_script,no_timestep_,thermo_freq,md_timestep,i_,j_,box_size_bar,box_size_index,Path_2_shell_scirpts,Path_2_generic,fluid_name,SRD_MD_ratio_,dump_freq_)
        
+# %% producing scripts for myriad
+Path_2_shell_scirpts='/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/Shell_scripts_for_MYRIAD'
+#MYRIAD path
+abs_path_2_lammps_exec='/home/ucahlrl/simulation_run_folder/lammps-23Jun2022/build_MYRIAD_Pure_SRD/lmp_mpi'
+abs_path_2_lammps_script='/home/ucahlrl/simulation_run_folder/in.pure_srd_nonequilibrium_hdf5_dump_full'
+
+Path_2_generic='/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/Shell_scripts_for_MYRIAD'
+data_transfer_instructions=''
+extra_code=''
+wd_path='/home/ucahlrl/Scratch/output/'
+num_task_req=''
+tempdir_req=''
+##################
+### Variables settings
+# calculating possible SRD_MD ratios and MD timesteps 
+SRD_MD_ratio_ = 10
+md_timestep=collision_time_negative_bar/SRD_MD_ratio_
+#no_timestep_=(10000000*(SRD_MD_ratio_/SRD_MD_ratio_[0])).astype('int')
+no_timestep_=np.array([100000])
+print(md_timestep)
+print(collision_time_negative_bar)
+dump_freq_=10
+thermo_freq=1000
+erate_=np.array([0.002,0.001,0.0005,0.0001])
+VP_ave_freq=10000
+i_=0
+j_=3
+Path_2_generic='/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/Shell_scripts_for_MYRIAD'
+
+box_size_index=7
+fluid_name='M_'+str(fluid_particle_number_density)+'_L_'+str(box_size_bar[box_size_index])+'_'
+
+
+
+realisation_index_=[1,2,3]
+
+tempdir_req='100G'
+ram_requirement='20G'
+wall_time='4:00:00'
+np_req=str(8)
+phi_= str(phi[box_size_index])
+
+
+var_choice_1=erate_
+var_choice_2=no_timestep_
+
+
+
+def sim_file_prod_fix_deform_pure_individual_MYRIAD(nu_bar,erate_,var_choice_1,var_choice_2,data_transfer_instructions,extra_code,wd_path,np_req,num_task_req,tempdir_req,wall_time,ram_requirement,realisation_index_,VP_ave_freq,abs_path_2_lammps_exec,abs_path_2_lammps_script,no_timestep_,thermo_freq,md_timestep,i_,j_,box_size_bar,box_size_index,Path_2_shell_scirpts,Path_2_generic,fluid_name,SRD_MD_ratio_,dump_freq_):
+    
+    os.chdir(Path_2_shell_scirpts)
+    META_DATA = str(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))
+    specific_email = 'luke.debono.21@ucl.ac.uk'
+    simulation_batch_folder= 'simulation_batch_scripts_pure_fix_deform_mpc_visc_'+str(nu_bar)+'_box_size_'+str(box_size_bar[box_size_index])+'_gdot_'+str(erate_[0])+'_'+META_DATA
+    os.mkdir(simulation_batch_folder)
+    sim_batchcode=str(np.random.randint(0, 1000000))
+    # test to check consistency of cores request 
+    
+    
+    for j in range(i_,j_): #or now just use one realisation 
+        for k in range(0,var_choice_1.size):       
+                for m in range(0,var_choice_2.size):#range(0,1):  
+                        param_set_code=str(np.random.randint(0, 1000000))
+                        simulation_run_name=fluid_name+'_pure_fix_deform_mpc_visc_'+str(nu_bar)+'_'+str(sim_batchcode)+'_'+param_set_code+'_realisation_'+str(j)+'_erate_'+str(var_choice_1[k])+'_timestep_'+str(md_timestep)+'_no_timesteps_'+str(no_timestep_[m])+'_SRDMDratio_'+str(SRD_MD_ratio_)+'_'
+                        run_code=''
+                        no_SRD=str(int(srd_count[box_size_index])) 
+                        #print(no_SRD)
+                        box_size = str(box_size_bar[box_size_index])
+                        timestep_input= str(md_timestep)
+                        chunk = 20 # number of chunks to use for VP averaging
+                        SRD_MD_ratio=str(SRD_MD_ratio_)
+                        lamda= str(collision_time_negative_bar)
+                        dump_freq=str(dump_freq_) # 
+                        thermo_freq = str(thermo_freq) # 
+                        temp_=str(1)
+                        grid_size=str(1)
+                        mass_SRD=str(1)
+                        no_timesteps = str(no_timestep_[m])
+                        rand_int =str(np.random.randint(0, 1000000))
+                        rand_int_1 =str( np.random.randint(0, 1000000))
+                        rand_int_2 =str(np.random.randint(0, 1000000))
+                        num_proc=str(np_req)
+                        erate=str(erate_[m])
+                        
+
+
+                        run_code_individual ='mpirun -np '+str(num_proc)+'  '+abs_path_2_lammps_exec+' -var erate_in '+str(erate)+'  -var fluid_name '+fluid_name +' -var  sim_batchcode '+str(sim_batchcode)+'  -var VP_ave_freq '+str(VP_ave_freq)+' -var chunk '+str(chunk)+' -var grid_size '+grid_size+' -var realisation_index '+str(realisation_index_[j])+' -var temp_ '+temp_+'  -var lambda '+str(lamda)+' -var rand_int '+rand_int+' -var rand_int_1 '+rand_int_1+' -var rand_int_2 '+rand_int_2+' -var no_SRD '+no_SRD+' -var mass_SRD '+mass_SRD+' -var box_size '+box_size+' -var timestep_input '+timestep_input+' -var SRD_MD_ratio '+SRD_MD_ratio+' -var dump_freq '+dump_freq+' -var thermo_freq '+thermo_freq+' -var no_timesteps '+no_timesteps+' -in '+abs_path_2_lammps_script+' \n &'  #>> '+prod_run_file_name+' & \n'
+                                            # -var erate_in 0 -var fluid_name equilibrium -var  sim_batchcode 806324 -var swap_rate 15 -var swap_number 1 -var VP_ave_freq 10000 -var equilibration_timesteps 1000 -var chunk 20 -var grid_size 1 -var realisation_index 2 -var temp_ 1 -var lambda 0.05071624521210362 -var rand_int 1234 -var rand_int_1 5678 -var rand_int_2 13565 -var no_SRD 60835 -var mass_SRD 1 -var box_size 23.0 -var timestep_input 0.005071624521210362 -var SRD_MD_ratio 10 -var dump_freq 1000 -var thermo_freq 1000 -var no_timesteps 600000 -var vel_target 10
+
+
+                        #print(run_code_individual)
+                        run_code=run_code +run_code_individual
+
+
+                        run_code = run_code[:-3]
+
+                        py2bash_launch_overwriter.py2bash_launch_overwriter(Path_2_generic,simulation_batch_folder,simulation_run_name,specific_email,wall_time,ram_requirement,tempdir_req,num_task_req,np_req,wd_path,extra_code,run_code,data_transfer_instructions)
+
+#%%
+sim_file_prod_fix_deform_pure_individual_MYRIAD(nu_bar,erate_,var_choice_1,var_choice_2,data_transfer_instructions,extra_code,wd_path,np_req,num_task_req,tempdir_req,wall_time,ram_requirement,realisation_index_,VP_ave_freq,abs_path_2_lammps_exec,abs_path_2_lammps_script,no_timestep_,thermo_freq,md_timestep,i_,j_,box_size_bar,box_size_index,Path_2_shell_scirpts,Path_2_generic,fluid_name,SRD_MD_ratio_,dump_freq_)
 # %%
